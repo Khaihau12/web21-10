797c39ded4a37defeba6c755f91a75616a964599
Khaihau12 <khaihau100@gmail.com>
2025-11-02 21:17:16 +0700

    Create delete_category.php

---
+++ admin/delete_category.php
@@ -0,0 +1,26 @@
+<?php
+require_once "dbadmin.php";
+// Kiểm tra đăng nhập
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $category_id = isset($_POST['category_id']) ? intval($_POST['category_id']) : 0;
+    if ($category_id > 0) {
+        $db = new dbadmin();
+        $result = $db->delete('categories', ['category_id' => $category_id]);
+        if ($result) {
+            // Quay về trang danh sách chuyên mục
+            header("Location: category_list.php?msg=success");
+            exit;
+        } else {
+            echo "Xóa loại tin thất bại!";
+        }
+    } else {
+        echo "ID không hợp lệ!";
+    }
+} else {
+    echo "Phương thức không hợp lệ!";
+}
+?>
