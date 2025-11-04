e817014aa4a56cb1dcf00f7c5e8bc6758b897429
Khaihau12 <khaihau100@gmail.com>
2025-11-04 16:09:21 +0700

    Update delete_category.php

--- admin/delete_category.php
+++ admin/delete_category.php
@@ -3,7 +3,12 @@
 require_once 'check_login.php';
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    $category_id = isset($_POST['category_id']) ? intval($_POST['category_id']) : 0;
+    if (isset($_POST['category_id'])) {
+        $category_id = intval($_POST['category_id']);
+    } else {
+        $category_id = 0;
+    }
+    
     if ($category_id > 0) {
         $db = new dbadmin();
         $result = $db->delete('categories', ['category_id' => $category_id]);
